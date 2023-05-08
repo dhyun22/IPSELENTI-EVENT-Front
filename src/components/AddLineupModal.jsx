b3f28000109ea6793f44dd4a839dd53bbdcdec14
noviceo <2000robert@naver.com>
2023-05-08 16:50:41 +0900

    임시 커밋

--- src/components/AddLineupModal.jsx
+++ src/components/AddLineupModal.jsx
@@ -7,15 +7,16 @@ function AddLineupModal() {
     return (
             <div>
                 <button onClick={() => setModalOpen(true)}>Modal Open</button>
-                <Modal className='lineupModal' isOpen={modalOpen} onRequestClose={() => setModalOpen}>
-                    <div className='closeContainer'>
-                        <GrClose className='modalClose' onClick={() => setModalOpen(false)} />
-                    </div>
+                <Modal 
+                className='lineupModal'
+                isOpen={modalOpen}
+                onRequestClose={() => setModalOpen}>
+                    <GrClose className='modalClose' onClick={() => setModalOpen(false)} />
                     <div className='singerContainer'>
                         <h3>가수 이름</h3>
                         <input className='singerInput'></input>
                         <h3>신청사유(선택)</h3>
-                        <input className='reasonInput'></input>
+                        <textarea className='reasonInput'></textarea>
                     </div>
                     <button className='addLineupBtn'>라인업 추가</button>
                 </Modal>
