5e28c4c7ed02344379e22a95ce0977b93554e78d
noviceo <2000robert@naver.com>
2023-05-09 00:37:29 +0900

    feat: shareModal 추가

--- src/components/ShareModal.jsx
+++ src/components/ShareModal.jsx
@@ -1,9 +1,14 @@
 import Modal from 'react-modal';
 import { useState } from 'react';
 import { GrClose } from 'react-icons/gr';
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function ShareModal() {
     const [modalOpen, setModalOpen] = useState(false);
+    const [buttonText, setButtonText] = useState('복사하기');
+    const handleClick = () => {
+        setButtonText('복사완료');
+    }
     return (
         <div>
             <button onClick={() => setModalOpen(true)}>Modal Open</button>
@@ -12,10 +17,17 @@ function ShareModal() {
             isOpen={modalOpen}
             onRequestClose={() => setModalOpen}>
                 <GrClose className='modalClose' onClick={() => setModalOpen(false)} />
-                <div>
+                <div className='shareContainer'>
                     <h2>공유</h2>
-                    
+                    <input className='shareLink' placeholder='www.asku.wiki/lineup_event' disabled></input>
                 </div>
+                <CopyToClipboard text='www.asku.wiki/lineup_event'>
+                    <button
+                    onClick={handleClick}
+                    className='copyBtn'>
+                        {buttonText}
+                    </button>
+                </CopyToClipboard>
             </Modal>
         </div>
     );
