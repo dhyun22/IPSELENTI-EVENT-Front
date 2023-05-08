5b9055434f44107713a1fb60d37a3c0be13ff5dc
noviceo <2000robert@naver.com>
2023-05-08 17:39:56 +0900

    임시 커밋

--- src/components/AddLineupModal.jsx
+++ src/components/AddLineupModal.jsx
@@ -5,8 +5,7 @@ import { GrClose } from 'react-icons/gr';
 function AddLineupModal() {
     const [modalOpen, setModalOpen] = useState(false);
     return (
-        <div className="container">
-            <div className="mobile-view">
+            <div>
                 <button onClick={() => setModalOpen(true)}>Modal Open</button>
                 <Modal className='lineupModal' isOpen={modalOpen} onRequestClose={() => setModalOpen}>
                     <div className='closeContainer'>
@@ -17,11 +16,11 @@ function AddLineupModal() {
                         <input className='singerInput'></input>
                         <h3>신청사유(선택)</h3>
                         <input className='reasonInput'></input>
-                        <button className='redbutton'>라인업 추가</button>
+                        <button className='addlineupBtn'>라인업 추가</button>
                     </div>
+                    
                 </Modal>
             </div>
-        </div>
     );
 };
 
