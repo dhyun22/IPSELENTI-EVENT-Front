44f0ca3a65f019e8e1c3b790389ec52344a541da
noviceo <2000robert@naver.com>
2023-05-09 17:35:07 +0900

    fix: signupCompleted

--- src/pages/SignUpCompleted.jsx
+++ src/pages/SignUpCompleted.jsx
@@ -1,4 +1,6 @@
-import temporaryLogo from '../img/temporaryLogo.png';
+import logo from '../img/logo.png';
+import { Link } from 'react-router-dom';
+import editCharacter from '../img/haho-head.png';
 
 function SignUpCompleted() {
     const signUpText = "회원가입이 완료되었습니다!\n이제 함께 입실렌티를 즐겨 보아요!"
@@ -6,10 +8,17 @@ function SignUpCompleted() {
         <div className='container'>
             <div className='mobile-view'>
                 <div className='editResult'>
-                    <img className='temporaryLogo' src={temporaryLogo} alt='temporary_logo' />
-                    <p>{signUpText}</p>
-                    <button className="signUpCompleted">로그인 하기</button>
-                    <button className="backToHome">홈 화면으로 이동</button>
+                    <img className='editLogo' src={logo} alt='logo' />
+                    <img className='editCharacter' src={editCharacter} alt='haho' />
+                    <div className='textContainer'>
+                        <p style={{fontSize: '14px'}}>{signUpText}</p>
+                    </div>
+                    <Link to='/login'>
+                        <button className="signUpCompleted">로그인 하기</button>
+                    </Link>
+                    <Link to='/'>
+                        <button className="backToHome">홈 화면으로 이동</button>
+                    </Link>
                 </div>
             </div>
         </div>
