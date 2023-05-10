82f8e9337de40982bcf11ff8c1f769e81696031d
noviceo <2000robert@naver.com>
2023-05-10 13:28:21 +0900

    feat: mainpage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import { Link } from 'react-router-dom';
+import homeCharacter from '../img/haho_standing.png';
 function Home() {
     return (
 
@@ -9,6 +10,11 @@ function Home() {
                         <Header />
                     </div>
                     <div className="body">
+                        <p className="homeText1">올해는 누구 오려나..</p>
+                        <p className="homeText2">입장은 몇시부터?</p>
+                        <p className="homeText3">뭐 입고 가지?</p>
+                        <p className="homeText4">점심은 어떡하지?</p>
+                        <img className='homeCharacter' src={homeCharacter} alt="haho" />
                         <Link to='/wiki'>
                             <button className="redbutton">입실렌티 함께하기!</button>
                         </Link>
