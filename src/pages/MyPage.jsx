f2330ffd344203c1792b2299f81b82096249b4c1
dhyun22 <sandy2873@naver.com>
2023-05-22 03:09:26 +0900

    마이페이지 axios 수정

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -40,12 +40,15 @@ function MyPage() {
               takeuser(); //로그인 성공 시에만 불러옴
           } else if (response.status === 401){
               setLoggedIn(false);
-              Navigate('/login');
+              navRef.current.navigate('/login');
           }
       } catch (error) {
           console.error(error);
       }
     }
+
+    checkLoginStatus();
+  }, []);
 
     const takeuser = async () => {
       try{
@@ -62,9 +65,7 @@ function MyPage() {
       }
     } 
       
-    checkLoginStatus();
-
-  }, []);
+    
 
 
   
