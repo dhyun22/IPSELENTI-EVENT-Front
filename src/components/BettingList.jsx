91429440276b8b7fb35ddb6868dfb78340cd617e
dhyun22 <sandy2873@naver.com>
2023-05-21 19:34:40 +0900

    배팅 내역 수정

--- src/components/BettingList.jsx
+++ src/components/BettingList.jsx
@@ -1,50 +1,45 @@
-import React, {useEffect} from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
 function BettingList() {
+  const [betHistory, setBetHistory] = useState([]);
+  const [celebrities, setCelebrities] = useState([]);
 
-  const [betHistory, setbetHistory] = useState([]);
-  const [celebrities, setCelebrities]=useState([]);
+  const takeBet = async () => {
+    try {
+      const historylist = await axios.get(process.env.REACT_APP_HOST + "/user/mypage/bettinghistory", { withCredentials: true });
 
-  const takebet=async() => {
-    try{
- 
-      const historylist= await axios.get(process.env.REACT_APP_HOST+"/user/mypage/bettinghistory", {withCredentials:true});
+      if (historylist.data) {
+        setBetHistory(historylist.data.bettingHistory);
 
-      if (historylist.data){ 
-        setbetHistory(historylist.data.bettingHistory)
         const celebrityIds = historylist.data.bettingHistory.map(history => history.celebrity_id);
         const celebritiesResponse = await axios.get(process.env.REACT_APP_HOST + "/event/celebrities");
         const matchedCelebrities = celebritiesResponse.data.celebrities.filter(celebrity => celebrityIds.includes(celebrity.celebrity_id));
-  
+
         setCelebrities(matchedCelebrities);
       }
-
     } catch (error) {
       console.error(error);
     }
   }
- 
-  useEffect(()=> {takebet();}, []);
 
+  useEffect(() => {
+    takeBet();
+  }, []);
 
-  return(
-      <ul className='betting_list' id='betting_list'>
-        {betHistory.map((historylist, index) => (
-          <li key={index}>
-             <span>{historylist.celebrity_id}</span>
-             {celebrities.map(celebrity => {
-              if (celebrity.celebrity_id === historylist.celebrity_id) {
-              return <span>&nbsp;&nbsp;&nbsp;&nbsp;{celebrity.celebrities_name}</span>;
-             }
-              return null;
-              })}         
+  return (
+    <ul className='betting_list' id='betting_list'>
+      {betHistory.map((historylist) => {
+        const matchedCelebrity = celebrities.find(celebrity => celebrity.celebrity_id === historylist.celebrity_id);
+
+        return (
+          <li key={historylist.betting_id}>
+            {matchedCelebrity && <span>{matchedCelebrity.celebrities_name}</span>}
+            <span>{historylist.betting_point}</span>
           </li>
-          ))
-        }
-      </ul>
-  
+        );
+      })}
+    </ul>
   );
 };
 
